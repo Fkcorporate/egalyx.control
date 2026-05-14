8d767001f3e0e20f244e7430dc6ebc3f68e49de4
Fkcorporate <contact.fkcorporate@gmail.com>
2026-05-14 17:13:28 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-from datetime import datetime, date, timezone
+from datetime import datetime, date, timezone, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
